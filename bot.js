87a6d11848947773a16b7bb52fc66e3a34a3ffa7
Hext <38295542+kla123101k@users.noreply.github.com>
2018-10-29 18:03:31 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,5 +17,28 @@ client.on('message', message => {
   	}
 });
 
+onst Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    var canal = message.guild.channels.find('name', '💢punições');
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if (!member)
+        return message.reply("Usuário não encontrado.");
+    let reason = args.slice(1).join(' ');
+    if (!reason) reason = "Não informado.";
+    let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(message.author.avatarURL)
+        .setTitle(`<a:usargif:496053335422664726> | Reportes `)
+        .addField('Quem reportou:', message.author)
+        .addField('\n\n Quem foi reportado:', member.user)
+        .addField('\n\n Motivo:', reason)
+        .setFooter(`Enviada por ${message.author.username}.`)
+        .setTimestamp()
+    canal.send(embed);
+}
+
+
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
